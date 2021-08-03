a2251e04b573db7e970baa9bc7e233b0819bea85
GianTempo <giantemporetti@gmail.com>
2021-08-03 19:53:33 -0300

    Created homepage component and migrated login page to pages folder
    Created Home component which will contain the main page of the app.
    Moved Login component to Pages folder to mantain project's structure consistent.
    Created navBar component.
    Created routes for Home and Login. Empty route ("/") will redirect to login for now.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Import components to set Routes
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo:"/login"
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
