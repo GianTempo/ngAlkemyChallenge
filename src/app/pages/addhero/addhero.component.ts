22367367d97fa41d6285e4da66db5f269f02e4f5
GianTempo <giantemporetti@gmail.com>
2021-08-07 17:19:39 -0300

    Added herocard to addhero page
    Added herocard import to addhero page's template.
    Added a card to let user know that the hero was succesfully added to the team.
    Added a method to addhero page to add a hero to the team when is selected.
    Minor styles changes to mainpage and added the correspondant card type to the cards.
    Minor change to button in addheroform since it's inner text wasn't correct.

--- src/app/pages/addhero/addhero.component.ts
+++ src/app/pages/addhero/addhero.component.ts
@@ -11,11 +11,16 @@ export class AddheroComponent implements OnInit {
   constructor(private herosvc: HeroService) { }
 
   heroes: any[]
+  success:boolean = false;
 
   ngOnInit(): void {
   }
 
   searchHero(e: any) {
     this.herosvc.searchHeroe(e).then(res => {this.heroes = res.data.results});
+  }
+
+  addHero(e: any) {
+    this.herosvc.getHeroe(e).then(res => {this.herosvc.addHeroe(res.data), this.success = true});
   }
 }
